e81b3dbe0b7c42822263bc998efbe531b3544abe
Tobias Kim <madeffort0903@gmail.com>
2024-06-19 00:26:39 +0900

    home html scratch

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third party libraries
-    "drf_yasg",
     "pwa",
     # apps
     "apps.caption",
--- config/urls.py
+++ config/urls.py
@@ -16,25 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Caption Maker API",
-        default_version="v1",
-        description="Caption Maker",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # swagger
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
     # pwa
     path('', include('pwa.urls')),
     path('', include('apps.caption.urls')),
