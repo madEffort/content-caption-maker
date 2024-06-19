db2ade2c4f4bf0794c3234f583648a7e9fc51e87
Tobias Kim <madeffort0903@gmail.com>
2024-06-19 10:56:48 +0900

    progressbar html done .

--- apps/caption/urls.py
+++ apps/caption/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "caption"
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('transcribe_video/', views.transcribe_video, name='transcribe_video'),
